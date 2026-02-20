e0c6981406a9ac51b6c3f9fb7331ae219b5679b7
shaheerbits <shaheercodes.allday@gmail.com>
2026-02-21 01:07:21 +0530

    implement welcome

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,36 +2,38 @@ import dayjs from "dayjs";
 import { navLinks, navIcons } from "@constants";
 
 const Navbar = () => {
-  return (
-    <nav>
-      <div>
-        <img src="images/logo.svg" alt="logo" />
-        <p className="font-bold">Shaheer's Portfolio</p>
+    return (
+        <nav>
+            <div>
+                <img src="images/logo.svg" alt="logo" />
+                <p className="font-bold">Shaheer's Portfolio</p>
 
-        <ul>
-          {navLinks.map(({ id, name }) => (
-            <li key={id}>
-              <p>{name}</p>
-            </li>
-          ))}
-        </ul>
-      </div>
+                <ul>
+                    {navLinks.map(({ id, name }) => (
+                        <li key={id}>
+                            <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
 
-      <div>
-        <ul>
-            {
-                navIcons.map(({id, img}) => (
-                    <li key={id}>
-                        <img src={img} className="icon-hover" alt={`image-${id}`} />
-                    </li>
-                ))
-            }
-        </ul>
-        
-        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
-      </div>
-    </nav>
-  );
+            <div>
+                <ul>
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img
+                                src={img}
+                                className="icon-hover"
+                                alt={`image-${id}`}
+                            />
+                        </li>
+                    ))}
+                </ul>
+
+                <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
